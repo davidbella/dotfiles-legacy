9f022f28ee46280f7e300902cb4b22fc04b5ca93
David Bella <dbella1@Davids-MacBook-Air.local>
2014-10-13 21:49:12 -0400

    Start tracking gitconfig for git shortcuts

--- link.sh
+++ link.sh
@@ -1,5 +1,6 @@
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.scripts/ ~/.scripts
 ln -s ~/dotfiles/.vim/ ~/.vim
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
